d99e4631044c63f4865b4b0d07eba1bfe8cabdba
SheikBazith <83687604+SheikBazith@users.noreply.github.com>
2024-07-09 22:49:30 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Intro(props) {
   return (
-    <div id='home' className='flex justify-center items-center h-[100vh] bg-[#fdb854]'>
-      <div className='flex flex-col justify-center items-center'>
-        <h2 className='text-8xl leading-[6.5rem] text-[#292929] dark:text-white'>SHEIK BAZITH</h2>
-        <p className='text-4xl text-[#292929] dark:text-white'>Designer X Developer</p>
+    <div id='home' className='flex items-center justify-center bg-[#fdb854]'>
+      <div className='flex flex-col justify-center items-center min-h-[100vh] w-[60%]'>
+        <h2 className='text-4xl sm:text-6xl leading-[3.5rem] text-[#292929] dark:text-white'>SHEIK BAZITH</h2>
+        <p className='text-[#292929] dark:text-white'>Designer X Developer</p>
 
       </div>
       </div>
