2f1d9c055c9f2d325cd43262a5694e75a2c8b66c
SheikBazith <83687604+SheikBazith@users.noreply.github.com>
2024-07-14 16:13:04 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,27 +3,26 @@ import React, { Component } from 'react'
 export class Contact extends Component {
   render() {
     return (
-      <div id='contact' className='flex flex-col justify-center items-center gap-12 p-12'>
-      <h1 className='  text-6xl dark:text-white'>Contact</h1>
-      <div className='contact-box border-[1px] rounded-3xl p-12 flex flex-col gap-12 bg-[#eeeade] dark:bg-[#29cb8c] dark:text-black'>
-      <div className=''>
-      <img className='w-[800px]' src='images/extra/contact.png'/>
+      <div id='contact' className='flex flex-col gap-6 justify-center items-center mb-5 pb-12 pt-20 min-h-[100vh]'>
+      <h1 className=' text-4xl dark:text-white'>Contact</h1>
+      <div className='contact-box max-w-[660px] flex flex-col md:flex-row justify-between gap-12 items-center p-4 bg-[#eeeade] dark:bg-[#29cb8c] rounded-3xl text-white dark:text-black'>
+      <div className='bg-white rounded-xl'>
+      <img className='w-[300px] rounded-xl' src='images/extra/contact.png'/>
       </div>
-      <div className='flex flex-col gap-6'>
-          <div className='flex items-center gap-4 bg-white p-6 rounded-full cursor-pointer hover:bg-slate-200 hover:border-[2px] hover:border-black transition-all'>
-            <img className='w-[120px]' src='images/extra/email.png'/>
-            <p className='text-5xl'>shkbaz@gmail.com</p>
+      <div className='flex flex-col gap-5'>
+          <div className='flex items-center gap-4 bg-white p-2 pr-16 rounded-full'>
+            <img className='w-[40px]' src='images/extra/email.png'/>
+            <p className='text-black'>shkbaz@gmail.com</p>
           </div>
 
-          <div className='flex items-center gap-4 bg-white p-6 rounded-full cursor-pointer hover:bg-slate-200 hover:border-[2px] hover:border-black transition-all'>
-            <img className='w-[120px]' src='images/extra/phone.png'/>
-            <p className='text-5xl'>+91 7406010515</p>
+          <div className='flex items-center gap-4 bg-white p-2 pr-16 rounded-full'>
+            <img className='w-[40px]' src='images/extra/phone.png'/>
+            <p className='text-black'>+91 7406010515</p>
           </div>
 
-          <button onClick={() => window.open('https://drive.google.com/u/0/uc?id=1E17xZP8Dza3FhA6Zb83JREQeDUDb9vnM&export=download', '_blank')}>
-          <div className='flex items-center gap-4 bg-white p-6 rounded-full cursor-pointer hover:bg-slate-200 hover:border-[2px] hover:border-black transition-all'>
-            <img className='w-[120px]' src='images/extra/resume.png'/>
-            <p className='text-5xl'>Download Resume</p>
+          <button onClick={() => window.open('https://drive.google.com/u/0/uc?id=1E17xZP8Dza3FhA6Zb83JREQeDUDb9vnM&export=download', '_blank')}><div className='flex items-center gap-4 bg-white p-2 pr-16 rounded-full cursor-pointer hover:bg-slate-200 hover:border-[2px] hover:border-black transition-all'>
+            <img className='w-[40px]' src='images/extra/resume.png'/>
+            <p className='text-black'>Download Resume</p>
           </div></button>
 
   
