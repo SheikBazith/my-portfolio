affa8b7bb1a76961b69ed5d18876ef66912d7ca9
SheikBazith <83687604+SheikBazith@users.noreply.github.com>
2024-07-15 14:55:38 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sidebar(props) {
   return (
-    <div className='w-[60px] h-screen flex flex-col justify-center items-center fixed'>
+    <div className='w-[60px] h-screen flex flex-col justify-center items-center fixed z-50'>
     <div className='bg-[#5a4ff5] flex flex-col gap-3 p-3 rounded-r-xl'>
         <button onClick={() => window.open('https://github.com/SheikBazith', '_blank')}><img className='side-list' src='images/side-bar/light-mode/git.png' /></button>
         <button onClick={() => window.open('https://www.linkedin.com/in/sheik-bazith-439415205/', '_blank')}><img className='side-list' src='images/side-bar/light-mode/linkedin.png'/></button>
